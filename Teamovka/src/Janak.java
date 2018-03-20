ca93adf667e254a010d85243fcf68d969e17d4b5
janak <janak@PC528.SPSEJECNANET.CZ>
2018-03-20 11:37:50 +0100

    lets to rozdrbat Janak.java

--- Teamovka/src/Janak.java
+++ Teamovka/src/Janak.java
@@ -1,4 +1,16 @@
 
+import java.util.*;
+
 public class Janak {
+	Scanner sc = new scanner(System.in);	//vyžádání hesla
+	System.out.printl("Zadej heslo:"); 
+	string a = sc.nextLine();
+	
+	int ascii = (int) character;
+
+	String character = name.StringAt(0);
+	int ascii = (int) character; // ascii(heslo) je teï v decimálním kodu
+	
+	System.out.print(Integer.toBinaryString(ascii));	//pøevod z decimálního do binárního
 
 }
