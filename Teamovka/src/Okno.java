e231868f459ee209c584f665e8324e1443b5df95
makovec <makovec@PC530.SPSEJECNANET.CZ>
2018-03-20 11:21:10 +0100

    -updated okno

--- Teamovka/src/Okno.java
+++ Teamovka/src/Okno.java
@@ -6,13 +6,23 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JFileChooser;
 
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+	/**
+	 * 
+	 * @author makovec
+	 *
+	 */
 public class Okno extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -35,7 +45,7 @@ public class Okno extends JFrame {
 	 */
 	public Okno() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 185);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -49,11 +59,31 @@ public class Okno extends JFrame {
 		JButton btnSelectPath = new JButton("Zvol Cestu");
 		btnSelectPath.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			}
+				JFileChooser fc = new JFileChooser();
+				int retVal = fc.showOpenDialog(null);
+                if (retVal == JFileChooser.APPROVE_OPTION) {
+                    File selectedfile = fc.getSelectedFile();
+				
+				
+			}}
 		});
 		btnSelectPath.setBounds(310, 49, 114, 23);
 		contentPane.add(btnSelectPath);
+		
+		JLabel lblCestaSouboru = new JLabel("Cesta Souboru");
+		lblCestaSouboru.setBounds(10, 25, 114, 14);
+		contentPane.add(lblCestaSouboru);
+		
+		JButton btnZaifruj = new JButton("Za\u0161ifruj");
+		btnZaifruj.setBounds(310, 83, 114, 43);
+		contentPane.add(btnZaifruj);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(10, 106, 290, 20);
+		contentPane.add(passwordField);
+		
+		JLabel lblKodovaciKlic = new JLabel("Kodovac\u00ED Kl\u00ED\u010D");
+		lblKodovaciKlic.setBounds(10, 81, 290, 14);
+		contentPane.add(lblKodovaciKlic);
 	}
-	
-	
 }
